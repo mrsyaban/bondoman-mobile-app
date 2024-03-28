c7c466b7f73320b8601dfcde4b058a644ddbc77b
Rifqi222 <mrifki193@gmail.com>
2024-03-28 21:31:20 +0700

    add: add scan init

--- app/src/main/java/com/pbd/psi/ui/scan/ScanFragment.kt
+++ app/src/main/java/com/pbd/psi/ui/scan/ScanFragment.kt
@@ -1,60 +1,162 @@
 package com.pbd.psi.ui.scan
 
+import android.Manifest
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Handler
+import android.os.Looper
+import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.pbd.psi.R
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.pbd.psi.databinding.FragmentScanBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ScanFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ScanFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private var _binding: FragmentScanBinding? = null
+    private val binding get() = _binding!!
+
+    private var imageCapture: ImageCapture? = null
+
+    private var previewFrozen: Boolean = false
+    private var cameraProvider: ProcessCameraProvider? = null
+
+    private val filePickerLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val selectedImageUri = result.data?.data
+            selectedImageUri?.let { uri ->
+                loadSelectedImage(uri)
+            }
         }
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_scan, container, false)
+    ): View {
+        _binding = FragmentScanBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val permissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted ->
+            if (isGranted) {
+                startCamera()
+            }
+        }
+
+        permissionLauncher.launch(Manifest.permission.CAMERA)
+
+        imageCapture = ImageCapture.Builder().build()
+
+        binding.scanButton?.setOnClickListener {
+            previewMask()
+        }
+
+        binding.uploadImage?.setOnClickListener{
+            openFileExplorer()
+        }
+
+        return root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ScanFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ScanFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+        cameraProviderFuture.addListener({
+
+            cameraProvider = cameraProviderFuture.get()
+
+            val preview = Preview.Builder().build().also {
+                    mPreview ->
+                if (!previewFrozen) {
+                    mPreview.setSurfaceProvider(binding.scanView?.surfaceProvider)
+                } else {
+                    mPreview.setSurfaceProvider(null)
                 }
             }
+
+            imageCapture = ImageCapture.Builder().build()
+
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                cameraProvider?.unbindAll()
+                cameraProvider?.bindToLifecycle(this, cameraSelector, preview!!)
+            } catch (e: Exception) {
+                Log.d("CameraX", "startCamera Failed:", e)
+            }
+        }, ContextCompat.getMainExecutor(requireContext()))
+    }
+
+    private fun previewMask() {
+        binding.scanButton?.isEnabled = false
+        previewFrozen = !previewFrozen
+        startCamera()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            // TO DO: Add  modal
+            val builder: AlertDialog.Builder = AlertDialog.Builder(context)
+            builder
+                .setMessage("Do you want to retake the picture?")
+                .setPositiveButton("Yes") { dialog, which ->
+                    dialog.dismiss()
+                    binding.scanButton?.isEnabled = true
+                    previewFrozen = !previewFrozen
+                    startCamera()
+                }
+                .setNegativeButton("No") { dialog, which ->
+                    // TO DO : send a froze preview to the server as img form data
+                    dialog.dismiss()
+                }
+
+            val dialog: AlertDialog = builder.create()
+            dialog.show()
+        }, 1000)
+    }
+
+    private fun openFileExplorer() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        filePickerLauncher.launch(intent)
+    }
+
+    private fun loadSelectedImage(uri: Uri) {
+        try {
+            requireContext().contentResolver.openInputStream(uri)?.use { inputStream ->
+                val bitmap = BitmapFactory.decodeStream(inputStream)
+                if (bitmap != null) {
+                    val drawable = BitmapDrawable(resources, bitmap)
+                    binding.scanView?.background = drawable
+                } else {
+                    Toast.makeText(requireContext(), "Failed to decode image", Toast.LENGTH_SHORT).show()
+                    Log.e("ScanFragment", "Failed to decode image")
+                }
+            }
+        } catch (e: Exception) {
+            Toast.makeText(requireContext(), "Failed to load image", Toast.LENGTH_SHORT).show()
+            Log.e("ScanFragment", "Failed to load image", e)
+        }
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
